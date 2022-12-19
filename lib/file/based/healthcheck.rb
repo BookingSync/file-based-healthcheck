612383f9b3d9b42d28b968e87ccfcc2eb450a0c6
Karol Galanciak <karol.galanciak@gmail.com>
2022-12-19 13:17:45 +0100

    initial commit

---
+++ lib/file/based/healthcheck.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "healthcheck/version"
+
+module File
+  module Based
+    module Healthcheck
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/file/based/healthcheck/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module File
+  module Based
+    module Healthcheck
+      VERSION = "0.1.0"
+    end
+  end
+end
